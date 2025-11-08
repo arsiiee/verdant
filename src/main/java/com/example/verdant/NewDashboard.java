daead411f456fa11b058fcda9d411f3946d7b7ae
https://github.com/arsiiee/verdant.git <azsalcedo64143@liceo.edu.ph>
2025-11-08 12:52:12 +0800

    new dashboard update

--- src/main/java/com/example/verdant/NewDashboard.java
+++ src/main/java/com/example/verdant/NewDashboard.java
@@ -5,10 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class NewTodo extends Application {
+public class NewDashboard extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(DashboardFeature.class.getResource("new-todo.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(NewDashboard.class.getResource("new-dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
