6ace9b2cc326d502b494d5f002530e748b888426
https://github.com/arsiiee/verdant.git <azsalcedo64143@liceo.edu.ph>
2025-11-10 17:32:45 +0800

    new log update

--- src/main/java/com/example/verdant/LogController.java
+++ src/main/java/com/example/verdant/LogController.java
@@ -1,14 +1,26 @@
 package com.example.verdant;
 
+import com.calendarfx.view.CalendarView;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class LogController {
     @FXML
     private Label welcomeText;
+    private AnchorPane calendarContainer;
+    private CalendarView calendarView;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void initialize() {
+        calendarView = new CalendarView();
+        calendarView.showMonthPage();
+        calendarView.setPrefSize(960, 600);
+
+        calendarContainer.getChildren().add(calendarView);
+        AnchorPane.setTopAnchor(calendarView, 0.0);
+        AnchorPane.setLeftAnchor(calendarView, 0.0);
+        AnchorPane.setBottomAnchor(calendarView, 0.0);
+        AnchorPane.setRightAnchor(calendarView, 0.0);
     }
 }
